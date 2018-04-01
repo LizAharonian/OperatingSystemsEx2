b093c3827953ea1f70357e6100522e382c9e031d
LizAharonian <lizaharonian@gmail.com>
2018-04-01 17:13:41 +0300

    read input and making args arr

--- main.c
+++ main.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
+#include <memory.h>
+#include <malloc.h>
 
+#define INPUT_SIZE 100
+#define INPUT_PARAMS
 int main() {
-    while (1){
-        printf("prompt>");
+    // while (1) {
+    char input[INPUT_SIZE];
+    fgets(input, INPUT_SIZE, stdin);
+    printf("prompt>");
+    char args[INPUT_SIZE][INPUT_SIZE];
 
-        
-
-       /* if(){
-            break;
-        }*/
+    const char s[2] = " ";
+    char *token;
+    //get the first token
+    int i = 0;
+    token = strtok(input, s);
+    strcpy(args[i], token);
+    //walk through other tokens
+    while (token != NULL) {
+        printf(" %s\n", args[i]);
+        i++;
+        token= strtok(NULL, s);
+        strcpy(args[i], token);
     }
 
-    return 0;
+    return (0);
+
+
+    // }
 }
