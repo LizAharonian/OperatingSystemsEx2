e0cba94b358582b523710c9008f7c48c648dcfc5
LizAharonian <lizaharonian@gmail.com>
2018-04-01 17:22:56 +0300

    read input and making args arr

--- main.c
+++ main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <memory.h>
 #include <malloc.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <wait.h>
 
 #define INPUT_SIZE 100
-#define INPUT_PARAMS
+#define INPUT_PARAMS 10
 int main() {
     // while (1) {
+    printf("prompt>");
+
     char input[INPUT_SIZE];
     fgets(input, INPUT_SIZE, stdin);
-    printf("prompt>");
     char args[INPUT_SIZE][INPUT_SIZE];
 
     const char s[2] = " ";
@@ -22,8 +27,31 @@ int main() {
         printf(" %s\n", args[i]);
         i++;
         token= strtok(NULL, s);
-        strcpy(args[i], token);
+        if (token !=NULL) {
+            strcpy(args[i], token);
+        }
     }
+
+    //calling execv
+    /*int stat,waited,ret_code;
+    pid_t pid;
+    pid = fork();
+    if (pid == 0)
+    {  // Son
+        ret_code = execvp(args[0],args[1]);
+        if (ret_code == -1)
+        {
+            perror("exec failed ");
+            exit(-1);
+        }
+    }
+    else
+    {  *//* Father *//*
+        printf("Father: after fork, son proc id is %d \n",pid);
+        waited = wait(&stat);   *//* stat can tell what happened *//*
+        printf("Father: Son proc completed,  id is %d \n", waited);
+    }*/
+
 
     return (0);
 
