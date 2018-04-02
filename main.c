cbef50a7fc7295ae260f51202aa022d64d9b42f2
LizAharonian <lizaharonian@gmail.com>
2018-04-02 12:55:13 +0300

    after comments

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <wait.h>
 
+//defines
 #define INPUT_SIZE 1000
 #define JOBS_NUM 1000
 #define HOME "HOME"
@@ -12,69 +13,78 @@
 #define FAIL -1
 #define TRUE 1
 #define FALSE 0
+
+/**
+ * main function.
+ * runs the program
+ * @return 0
+ */
 int main() {
-    char jobs[INPUT_SIZE][INPUT_SIZE];
+    //allocate memory for jobs arrays
+    char jobs[JOBS_NUM][INPUT_SIZE];
     int pids[JOBS_NUM];
     int j =0;
-
+    //while loop runs the shell
     while (TRUE) {
         printf("prompt>");
         int isBackground = FALSE;
+        //allocate memory for input
         char input[INPUT_SIZE];
         char copyInput[INPUT_SIZE];
         //scan input from user
         fgets(input, INPUT_SIZE, stdin);
         //remove '/n'
         input[strlen(input) - 1] = '\0';
+        //copy input
         strcpy(copyInput,input);
-
+        //operate the action
         if (strcmp(input, "jobs")==0) {
             printJobs(pids,jobs,j);
-
         } else {
-
+            //allocate memory for args array
             char *args[INPUT_SIZE];
             makeArgs(args, input, &isBackground);
-
             if (strcmp(args[0],"cd")==0) {
                 cdImplementation(args);
             } else {
                 //calling execv
                 int pid = callExecv(args, isBackground);
-
+                //update pids array
                 pids[j] = pid;
-                // token = strtok(input, s);
                 strcpy(jobs[j], copyInput);
                 j++;
             }
-
         }
     }
     return (0);
-
 }
 
+/**
+ * callExecv function.
+ * @param args - array for execvp function
+ * @param isBackground - tells if parent need to wait to son
+ * @return pid of son
+ */
 int callExecv(char **args, int isBackground) {
-    int stat, waited, ret_code;
+    int stat;
     pid_t pid;
     pid = fork();
     if (pid == 0) {  // son
         execvp(args[0], &args[0]);
-
-
     } else {   //father prints pid of son
         printf("%d \n", pid);
         if (!isBackground) {
             wait(&stat);   // stat can tell what happened
-            int status = WEXITSTATUS(stat);
-            if (status == 1){
-                //fprintf(stderr, "exec failed\n");
-
-            }
         }
     }
     return pid;
 }
+
+/**
+ * cdImplementation function.
+ * @param args - input arguments
+ * @return success or failure
+ */
 int cdImplementation(char *args[]){
     if (args[1] ==NULL){
         chdir(getenv(HOME));
@@ -88,32 +98,18 @@ int cdImplementation(char *args[]){
     }
 }
 
-void updateJobes(int pids[], char *jobs[], int j) {
-    /*for (int i=0; i<j; i++) {
-        pid_t returnPid = waitpid(pids[i], NULL, WNOHANG);
-        if (returnPid ==0) {
-            //todo: check number of spaces!!
-
-            printf("%d         ", pids[i]);
-            int len = strlen(jobs[i]);
-            for (int k=0; k<len;k++){
-                if (!((k==len-1)&&jobs[i][k]=='&')){
-                    printf("%c", jobs[i][k]);
-
-                }
-            }
-        }
-    }*/
-}
-
-void printJobs(int pids[], char jobs[INPUT_SIZE][INPUT_SIZE], int j){
+/**
+ * printJobs function.
+ * @param pids - array of jobs pids
+ * @param jobs - array of jobs commands
+ * @param j - len of jobs array
+ */
+void printJobs(int pids[], char jobs[JOBS_NUM][INPUT_SIZE], int j){
     int flag =FALSE;
     for (int i=0; i<j; i++) {
         pid_t returnPid = waitpid(pids[i], NULL, WNOHANG);
         if (returnPid ==0) {
             flag=TRUE;
-            //todo: check number of spaces!!
-
             printf("%d         ", pids[i]);
             int len = strlen(jobs[i]);
             for (int k=0; k<len;k++){
@@ -128,6 +124,13 @@ void printJobs(int pids[], char jobs[INPUT_SIZE][INPUT_SIZE], int j){
         printf("\n");
     }
 }
+
+/**
+ * makeArgs function.
+ * @param args - array for execvp function
+ * @param input - user's input
+ * @param isBackground - tells if parent need to wait to son
+ */
 void makeArgs(char *args[INPUT_SIZE], char input[INPUT_SIZE], int* isBackground){
     const char s[2] = " ";
     char *token;
@@ -147,6 +150,4 @@ void makeArgs(char *args[INPUT_SIZE], char input[INPUT_SIZE], int* isBackground)
     }
     i++;
     args[i] = NULL;
-
-
 }
